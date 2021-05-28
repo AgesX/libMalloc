76d9d025b5456083be069af68f92e052d763e4ea
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-28 09:49:58 +0800

    //  x/4gx // x ,  打印内存情况

---
+++ proj/666/666/main.m
@@ -0,0 +1,76 @@
+//
+//  main.m
+//  666
+//
+//  Created by Jz D on 2021/5/28.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Warrior : NSObject
+
+
+
+@property (nonatomic, strong) NSString * weapon;
+
+
+@property (nonatomic, strong) NSString * shield;
+
+
+
+@end
+
+
+
+@implementation Warrior
+
+
+
+@end
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        
+        
+        
+        Warrior * k = [Warrior new];
+        
+        
+        
+        
+    }
+    return 0;
+}
+
+
+
+
+//  x/4gx
+// x ,  打印内存情况
+// 4 ， 打印 4 段
+
+// g,   格式化，输出打印
+// iOS， 小端模式，应该反向读
+
+
+// 最后的一个 x, 打印的意思
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
