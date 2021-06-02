089e107da01e0dc9f96d25d56bd79462afe47061
邓江洲 <dengjiangzhouReal@gmail.com>
2021-06-02 14:43:59 +0800

    //   看起来， copy 和 strong 修饰, 对于属性的 getter 方法，没啥影响

--- proj/001-类的属性与变量/001-类的属性与变量/简化版 main.cpp
+++ proj/001-类的属性与变量/001-类的属性与变量/简化版 main.cpp
@@ -53,7 +53,17 @@ struct LGPerson_IMPL {
 static NSString * _I_LGPerson_nickName(LGPerson * self, SEL _cmd) { return (*(NSString *__strong *)((char *)self + OBJC_IVAR_$_LGPerson$_nickName)); }
 extern "C" __declspec(dllimport) void objc_setProperty (id, SEL, long, id, bool, bool);
 
+
+
+
+//  属性 nickName， 用 copy 修饰
+
+
+
 static void _I_LGPerson_setNickName_(LGPerson * self, SEL _cmd, NSString *nickName) { objc_setProperty (self, _cmd, __OFFSETOFIVAR__(struct LGPerson, _nickName), (id)nickName, 0, 1); }
+
+
+
 
 
 
@@ -63,6 +73,22 @@ static void _I_LGPerson_setNickName_(LGPerson * self, SEL _cmd, NSString *nickNa
 
 static NSString * _I_LGPerson_name(LGPerson * self, SEL _cmd) { return (*(NSString *__strong *)((char *)self + OBJC_IVAR_$_LGPerson$_name)); }
 
+
+
+
+//  属性 name， 用 strong 修饰
+
+
 // 赋值
 static void _I_LGPerson_setName_(LGPerson * self, SEL _cmd, NSString *name) { (*(NSString *__strong *)((char *)self + OBJC_IVAR_$_LGPerson$_name)) = name; }
 // @end
+
+
+
+
+
+
+
+
+
+//   看起来， copy 和 strong 修饰, 对于属性的 getter 方法，没啥影响
